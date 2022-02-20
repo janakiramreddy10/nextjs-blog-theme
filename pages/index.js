115063e191761ffed2b752890ba5385a6a157a2d
janakiramreddy10 <janakiramreddy10@gmail.com>
2022-02-20 20:33:09 +0530

    test3

--- pages/index.js
+++ pages/index.js
@@ -17,6 +17,7 @@ export default function Index({ posts, globalData }) {
         <h1 className="text-3xl lg:text-5xl text-center mb-12">
           Teja I love you!...
         </h1>
+       </main>
     </Layout>
   );
 }
