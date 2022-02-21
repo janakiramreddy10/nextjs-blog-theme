c45a7bbe7cbfca43824ca1a72e56b938df7de973
janakiramreddy10 <janakiramreddy10@gmail.com>
2022-02-21 14:20:01 +0530

    test4

--- pages/index.js
+++ pages/index.js
@@ -14,7 +14,7 @@ export default function Index({ posts, globalData }) {
       <SEO title={globalData.name} description={globalData.blogTitle} />
       <Header name={globalData.name} />
       <main className="w-full">
-        <h1 className="text-3xl lg:text-5xl text-center mb-12">
+        <h1 className="text-3xl lg:text-5xl text-center mb-12 font-size: 20px; font-weight: bold; color: #2d38be;transition: 0.5s;">
           Teja I love you!...
         </h1>
        </main>
